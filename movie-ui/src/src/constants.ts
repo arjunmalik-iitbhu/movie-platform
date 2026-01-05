aeb6d525824e8e9dbfda3e857a861b739bed039d
arjunmalik09 <arjun.malikcd.cse14@iitbhu.ac.in>
2026-01-05 17:22:31 +0530

    add constants

---
+++ movie-ui/src/src/constants.ts
@@ -0,0 +1,17 @@
+export type ENTITY_TYPE = 'movie' | 'genre' | 'actor' | 'director';
+
+export interface Movie {
+  name: string
+}
+
+export interface Genre {
+  name: string
+}
+
+export interface Actor {
+  name: string
+}
+
+export interface Director {
+  name: string
+}
