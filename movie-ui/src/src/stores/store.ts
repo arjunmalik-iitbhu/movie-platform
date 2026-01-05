4577f2dfcb03735b21b0bfc97a77174e28b882b6
arjunmalik09 <arjun.malikcd.cse14@iitbhu.ac.in>
2026-01-05 17:20:34 +0530

    update store

--- movie-ui/src/src/stores/store.ts
+++ movie-ui/src/src/stores/store.ts
@@ -1,13 +1,12 @@
 import { defineStore } from 'pinia'
 import { config } from '../config'
-
-interface Movie {
-  name: string
-  age: number
-}
+import type { ENTITY_TYPE, Movie, Genre, Actor, Director } from '@/constants'
 
 interface Data {
   movies: Movie[]
+  genres: Genre[]
+  actors: Actor[]
+  directors: Director[]
 }
 
 interface Setting {
@@ -27,23 +26,46 @@ export const useInfoStore = defineStore('info', {
       },
       data: {
         movies: [],
+        genres: [],
+        actors: [],
+        directors: [],
       },
     }
   },
   actions: {
-    async fetchMovies() {
-      const resp = await fetch(`${config.API_BASE_URL}/movies`, {
+    async fetch(type: ENTITY_TYPE, params: {offset: number, limit: number, filters: {type: ENTITY_TYPE, value: string}[]}) {
+      const urlBase = `${config.API_BASE_URL}/${type}s`
+      const url = new URL(urlBase)
+      const processedparams = {
+        offset: String(params.offset),
+        limit: String(params.limit),
+        ...params.filters.reduce((curr, elem) => Object.assign(curr, {[elem.type]: elem.value}), {})
+      }
+      url.search = new URLSearchParams(processedparams).toString()
+      const resp = await fetch(url, {
         method: 'GET',
         headers: {},
       })
       if (!resp.ok) {
-        throw new Error(`Error in fetchMovies. Response status: ${resp.status}`)
+        throw new Error(`Error in fetch ${type}. Response status: ${resp.status}`)
       }
-      const movies: Movie[] = await resp.json()
-      this.data.movies = movies
+      const entities = await resp.json()
+      Object.assign(this.data, {[`${type}s`]: entities})
+    },
+    async add(type: ENTITY_TYPE, data: Movie | Genre | Actor | Director) {
+      const resp = await fetch(`${config.API_BASE_URL}/${type}s`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      if (!resp.ok) {
+        throw new Error(`Error in add ${type}. Response status: ${resp.status}`)
+      }
     },
     toggleNavBar() {
-      this.settings.navBarVisible = !this.settings.navBarVisible;
+      this.settings.navBarVisible = !this.settings.navBarVisible
     }
   },
 })
