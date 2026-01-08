7887d35f53efe93be68e33f9a31dd5c5337a6f73
arjunmalik09 <arjun.malikcd.cse14@iitbhu.ac.in>
2026-01-08 16:43:36 +0530

    update actions bar

--- movie-ui/src/src/components/ActionsBar.vue
+++ movie-ui/src/src/components/ActionsBar.vue
@@ -4,34 +4,40 @@ import { useInfoStore } from '@/stores/store'
 import AddIcon from '@/components/icons/IconAdd.vue'
 import MoreVertIcon from '@/components/icons/IconMoreVert.vue'
 import type { ENTITY_TYPE, EntityInterface } from '@/constants'
+import { toTitleCase } from '@/utilities'
 
 const DIALOG_CLASS = "add-entity"
 
-const ADD_ENTITY_FIELDS: Record<ENTITY_TYPE, {name: string, type: string, required: boolean}[]> = {
+const ADD_ENTITY_FIELDS: Record<ENTITY_TYPE, {prettyName: string, name: string, type: string, required: boolean}[]> = {
   movie: [
     {
+      prettyName: "Title",
       name: "title",
       type: "string",
       required: true
     },
     {
+      prettyName: "Release Year",
       name: "releaseYear",
       type: "number",
       required: true
     },
     {
+      prettyName: "Image Source",
       name: "imageSrc",
-      type: "number",
+      type: "string",
       required: false
     }
   ],
   genre: [
     {
+      prettyName: "Name",
       name: "name",
       type: "string",
       required: true
     },
     {
+      prettyName: "Image Source",
       name: "imageSrc",
       type: "string",
       required: false
@@ -39,11 +45,13 @@ const ADD_ENTITY_FIELDS: Record<ENTITY_TYPE, {name: string, type: string, requir
   ],
   actor: [
     {
+      prettyName: "Name",
       name: "name",
       type: "string",
       required: true
     },
     {
+      prettyName: "Image Source",
       name: "imageSrc",
       type: "string",
       required: false
@@ -51,11 +59,13 @@ const ADD_ENTITY_FIELDS: Record<ENTITY_TYPE, {name: string, type: string, requir
   ],
   director: [
     {
+      prettyName: "Name",
       name: "name",
       type: "string",
       required: true
     },
     {
+      prettyName: "Image Source",
       name: "imageSrc",
       type: "string",
       required: false
@@ -113,23 +123,31 @@ const addEntity = () => {
       </button>
       <div class="add-action-modal">
         <dialog :class="DIALOG_CLASS">
-          <h4>Add {{ entity }}</h4>
+          <h2>Add {{ toTitleCase(entity) }}</h2>
           <div class="add-entity-input" v-for="field in ADD_ENTITY_FIELDS[entity]" v-bind:key="field.name">
-            {{ field.name }} {{ field.required ? '': ' [optional]' }}
+            <p>{{ `${field.prettyName}` }} {{ field.required ? '': ' [optional]' }}</p>
             <input :class="`input-${field.name}`" :type="field.type" :required="field.required"/>
           </div>
-          <button
-            class="add-entity-button"
-            :disabled="false && ADD_ENTITY_FIELDS[entity].reduce(
-              (curr, elem) => (
-                curr || (elem.required && !Boolean(getEntityInput(`input-${elem.name}`)))
-              ),
-              false
-            )"
-            v-on:click="addEntity()"
-          >
-            Submit
-          </button>
+          <div class="add-entity-button">
+            <button
+              class="add-entity-submit-button"
+              :disabled="false && ADD_ENTITY_FIELDS[entity].reduce(
+                (curr, elem) => (
+                  curr || (elem.required && !Boolean(getEntityInput(`input-${elem.name}`)))
+                ),
+                false
+              )"
+              v-on:click="addEntity()"
+            >
+              Submit
+            </button>
+            <button
+              class="add-entity-close-button"
+              v-on:click="toggleAddEntity()"
+            >
+              Close
+            </button>
+          </div>
         </dialog>
       </div>
     </div>
@@ -165,6 +183,35 @@ const addEntity = () => {
   button:hover {
     cursor: pointer;
     background-color: var(--color-hover);
+  }
+  .add-action-modal {
+    display: flex;
+    flex-direction: column;
+    .add-entity {
+      position: fixed;
+      top: 20vh;
+      left: 40vw;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 1rem;
+      .add-entity-input {
+        display: flex;
+        flex-direction: column;
+        align-self: stretch;
+        align-items: space-between;
+        input {
+          display: flex;
+          flex: 1;
+        }
+      }
+      .add-entity-button {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+      }
+    }
   }
   .more-actions-menu {
     position: fixed;
