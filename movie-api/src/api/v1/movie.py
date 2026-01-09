77b0fbd53587bb3bb937627d35473808ca0d1073
arjunmalik09 <arjun.malikcd.cse14@iitbhu.ac.in>
2026-01-09 11:29:47 +0530

    update api

--- movie-api/src/api/v1/movie.py
+++ movie-api/src/api/v1/movie.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, HTTPException, Depends, status
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
 from src.deps import get_session
-from src.dto import MovieRes
+from src.dto import MovieRes, MovieCreateReq
 from src.model.entity import Movie
 
 router = APIRouter(
@@ -46,7 +46,8 @@ async def update_movie(movie_id: str, session: AsyncSession = Depends(get_sessio
 
 
 @router.post("/movie", response_model=Movie, status_code=status.HTTP_201_CREATED)
-async def create_movie(movie: Movie, session: AsyncSession = Depends(get_session)):
+async def create_movie(movieReq: MovieCreateReq, session: AsyncSession = Depends(get_session)):
+    movie = Movie(**movieReq.model_dump(by_alias=False))
     session.add(movie)
     await session.commit()
     await session.refresh(movie)
--- movie-api/src/dto/request.py
+++ movie-api/src/dto/request.py
@@ -1,8 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Optional
+from pydantic.alias_generators import to_camel
 
+class BaseSchema(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+        from_attributes=True
+    )
 
-class MovieCreateReq(BaseModel):
+class MovieCreateReq(BaseSchema):
     """
     Data Transfer Object for creating a new movie.
     """
