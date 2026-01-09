1321d09d132a79b8a3650310267a2ebe36921e6e
arjunmalik09 <arjun.malikcd.cse14@iitbhu.ac.in>
2026-01-09 13:34:25 +0530

    update movie ui

--- movie-ui/src/src/views/MovieView.vue
+++ movie-ui/src/src/views/MovieView.vue
@@ -2,7 +2,7 @@
 import ActionsBar from '@/components/ActionsBar.vue'
 import { useInfoStore } from '@/stores/store'
 import { onMounted } from 'vue';
-import { MOVIE } from '@/constants'
+import { MOVIE, DEFAULT_IMAGE } from '@/constants'
 
 const props = defineProps<{
   id: string
@@ -19,6 +19,7 @@ onMounted(async () => {
 <template>
   <main class="movie">
     <ActionsBar class="movie-actions" entity="movie"/>
+    <img :src="store.data[`all${MOVIE}s`][id]?.imageSrc || DEFAULT_IMAGE" />
     <h1 class="movie-title">{{ store.data[`all${MOVIE}s`][id]?.title }}</h1>
     <h5 class="movie-subtitle">{{ store.data[`all${MOVIE}s`][id]?.releaseYear }}</h5>
   </main>
@@ -29,6 +30,15 @@ onMounted(async () => {
   display: flex;
   flex-direction: column;
   flex: 1;
+  gap: 0.5rem;
+  img {
+    display: flex;
+    max-height: 40vh;
+    align-self: stretch;
+    opacity: 0.5;
+    border: 1px solid var(--color-border);
+    border-radius: 2rem;
+  }
   .movie-actions {
     display: flex;
     justify-content: flex-end;
@@ -36,6 +46,16 @@ onMounted(async () => {
   .movie-title {
     padding-top: 1rem;
     padding-bottom: 1rem;
+    position: fixed;
+    margin-top: 30vh;
+    margin-left: 5vw;
+  }
+  .movie-subtitle {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    position: fixed;
+    margin-top: 33vh;
+    margin-left: 5vw;
   }
   .movie-filter {
     display: flex;
